9acdab930f48052834cc001bcaff1210becdb290
Steven Chu <chu.chongmeng@gmail.com>
2018-12-28 10:12:34 +0800

    Initial commit

---
+++ activity.go
@@ -0,0 +1,70 @@
+package dispatch
+
+import (
+	//"encoding/json"
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+	"github.com/TIBCOSoftware/flogo-lib/logger"
+)
+
+var log = logger.GetLogger("activity-deviceType")
+
+const (
+	ivType = "type"
+	ivID = "id"
+	ivName = "name"
+	ivAction = "action"
+	ivValue = "value"
+)
+
+// MyActivity is a stub for your Activity implementation
+type Output struct {
+	Name string `json:"name"`
+	Action string `json:"action"`
+	Value float64 `json:"value"`
+}
+type MyActivity struct {
+	metadata *activity.Metadata
+}
+
+// NewActivity creates a new activity
+func NewActivity(metadata *activity.Metadata) activity.Activity {
+	return &MyActivity{metadata: metadata}
+}
+
+// Metadata implements activity.Activity.Metadata
+func (a *MyActivity) Metadata() *activity.Metadata {
+	return a.metadata
+}
+
+// Eval implements activity.Activity.Eval
+func (a *MyActivity) Eval(context activity.Context) (done bool, err error)  {
+	input := context.GetInput("input")
+	//output := Output{}
+	//json.Unmarshal([]byte(input), &output)
+
+	//output := make(map[string]interface{})
+	//json.Unmarshal(input, &output)
+	deviceType, ok := input.(map[string]interface{})[ivType].(string)
+	if !ok { log.Error("No type!") }
+	deviceId, ok := input.(map[string]interface{})[ivID].(string)
+	if !ok { log.Error("No id!") }
+	name, ok := input.(map[string]interface{})[ivName].(string)
+	if !ok { log.Error("No name!") }
+	action, ok := input.(map[string]interface{})[ivAction].(string)
+	if !ok { log.Error("No action!") }
+	value, ok := input.(map[string]interface{})[ivValue].(float64) 
+	if !ok { log.Error("No value!") }
+
+	log.Infof("input: %+v", input.(map[string]interface{}))
+	//log.Infof("[%s]", input["name"].(string))
+	//log.Infof("%+v", output)
+
+	context.SetOutput(ivType, deviceType)
+	context.SetOutput(ivID, deviceId)
+	context.SetOutput(ivName, name)
+	context.SetOutput(ivAction, action)
+	context.SetOutput(ivValue, value)
+	// do eval
+
+	return true, nil
+}
---
+++ activity_test.go
@@ -0,0 +1,69 @@
+package dispatch
+
+import (
+	"io/ioutil"
+	"testing"
+
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+	"github.com/TIBCOSoftware/flogo-contrib/action/flow/test"
+	"github.com/stretchr/testify/assert"
+)
+
+var activityMetadata *activity.Metadata
+
+func getActivityMetadata() *activity.Metadata {
+
+	if activityMetadata == nil {
+		jsonMetadataBytes, err := ioutil.ReadFile("activity.json")
+		if err != nil{
+			panic("No Json Metadata found for activity.json path")
+		}
+
+		activityMetadata = activity.NewMetadata(string(jsonMetadataBytes))
+	}
+
+	return activityMetadata
+}
+
+func TestCreate(t *testing.T) {
+
+	act := NewActivity(getActivityMetadata())
+
+	if act == nil {
+		t.Error("Activity Not Created")
+		t.Fail()
+		return
+	}
+}
+
+func TestEval(t *testing.T) {
+
+	defer func() {
+		if r := recover(); r != nil {
+			t.Failed()
+			t.Errorf("panic during execution: %v", r)
+		}
+	}()
+
+	act := NewActivity(getActivityMetadata())
+	tc := test.NewTestActivityContext(getActivityMetadata())
+
+	//setup attrs
+	tc.SetInput("input", `{"type":"button", "id":"1", "name":"b1", "action":"clicked", "value": -1.0}`)
+
+	act.Eval(tc)
+
+	deviceType := tc.GetOutput("type")
+	deviceId := tc.GetOutput("id")
+	name := tc.GetOutput("name")
+	action := tc.GetOutput("action")
+	value := tc.GetOutput("value")
+
+	assert.Equal(t, "button", deviceType)
+	assert.Equal(t, "1", deviceId)
+	assert.Equal(t, "b1", name)
+	assert.Equal(t, "clicked", action)
+	assert.Equal(t, -1.0, value)
+
+	//check result attr
+}
